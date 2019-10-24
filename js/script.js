9801a1be9206b265a1295ecc1f194d15684b0fd8
Caio <caiocraft2402@gmail.com>
2019-10-24 20:54:36 -0300

    jquery fancybox galeria (1.0 falha)

--- js/script.js
+++ js/script.js
@@ -15,3 +15,19 @@ $(function(){
         }    
     });    
 }); 
+
+$(document).ready(function(){
+    $(".fancybox").fancybox({
+          openEffect: "none",
+          closeEffect: "none"
+      });
+      
+      $(".zoom").hover(function(){
+          
+          $(this).addClass('transition');
+      }, function(){
+          
+          $(this).removeClass('transition');
+      });
+  });
+      
