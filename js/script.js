88fdfcdd98e41b25307a7f8195cd46283db9a863
Caio <caiocraft2402@gmail.com>
2019-11-06 17:37:58 -0300

    remoção do js fancybox

--- js/script.js
+++ js/script.js
@@ -15,19 +15,3 @@ $(function(){
         }    
     });    
 }); 
-
-$(document).ready(function(){
-    $(".fancybox").fancybox({
-          openEffect: "none",
-          closeEffect: "none"
-      });
-      
-      $(".zoom").hover(function(){
-          
-          $(this).addClass('transition');
-      }, function(){
-          
-          $(this).removeClass('transition');
-      });
-  });
-      
